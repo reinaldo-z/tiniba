13d9a76db28d5af2b9ac5a6c3da691d73ca9ba0b
Sean Anderson <sean.martin.anderson@gmail.com>
2014-05-20 22:59:22 +0200

    no need to compile for quads ATM

--- latm/SRC_1setinput/compiler_all.sh
+++ latm/SRC_1setinput/compiler_all.sh
@@ -3,7 +3,7 @@ P=$PWD
 # medusa&hexa
 make -f Makefilehexa clean; make -f  Makefilehexa 
 # quad
-ssh quad01    "cd $P; make -f Makefilequad clean; make -f Makefilequad"
+#ssh quad01    "cd $P; make -f Makefilequad clean; make -f Makefilequad"
 # itanium
 #ssh itanium01 "cd $P; make -f Makefile64b clean; make -f  Makefile64b"
 # xeon (needs to be modified)
